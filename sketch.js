2f034a5e8e3811c82f7348adc9ee646d0ac7d37a
Barbara Peric <b.peric10@gmail.com>
2024-03-25 12:38:44 +0100

    earth and moon

---
+++ sketch.js
@@ -0,0 +1,106 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require("three");
+
+// Include any additional ThreeJS examples below
+require("three/examples/js/controls/OrbitControls");
+
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  units: "in",
+  pixelsPerInch: 72,
+  scaleToView: true,
+  orientation: 'landscape',
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: "webgl"
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas
+  });
+
+  // WebGL background color
+  renderer.setClearColor("#000", 1);
+
+  // Setup a camera
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 500);
+  camera.position.set(3, 3, -5);
+  camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  // Setup a geometry
+  const geometry = new THREE.SphereGeometry(1, 32, 16);
+  const loader = new THREE.TextureLoader()
+
+  const texture = loader.load('earth.jpg')
+  const moonTexture = loader.load('moon.jpg')
+
+  // Setup a material
+  const material = new THREE.MeshStandardMaterial({
+    map: texture,
+    roughness: 1,
+    metalnes: 0
+  });
+
+  const moonMaterial = new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    roughness: 1,
+    metalnes: 0
+  });
+
+  // Setup a mesh with geometry + material
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const moonGroup = new THREE.Group()
+
+  const moonMesh = new THREE.Mesh(geometry, moonMaterial);
+  moonMesh.position.set(1.5, 1, 0);
+  moonMesh.scale.setScalar(0.25);
+
+  moonGroup.add(moonMesh);
+
+  scene.add(moonGroup)
+
+
+  const light = new THREE.PointLight("white", 2);
+  light.position.set(3, 3, -3)
+  scene.add(light)
+
+  // scene.add(new THREE.PointLightHelper(light, 0.1))
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      mesh.rotation.y = time * 0.15
+      moonMesh.rotation.y = time * 0.2;
+      moonGroup.rotation.y = time
+      controls.update();
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      controls.dispose();
+      renderer.dispose();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
